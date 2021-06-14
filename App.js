4b6e13344bec6196dbf7bd69878d00cee6ebfc81
jinghaochua <jinghao_chua@hotmail.com>
2021-06-14 10:39:01 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
