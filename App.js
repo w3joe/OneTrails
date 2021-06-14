6b41ec595abf88664b56a87f5ac9b4fe1e1289e1
jinghaochua <jinghao_chua@hotmail.com>
2021-06-14 11:27:27 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import EventsScreen from "./screens/EventsScreen";
+import ContactsScreen from "./screens/ContactsScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 
 const Tab = createBottomTabNavigator();
 
@@ -38,12 +43,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
