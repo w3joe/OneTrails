c1ec71699016d52e76de5a8916fdf775fd994bf5
jinghaochua <jinghao_chua@hotmail.com>
2021-06-14 18:03:23 +0800

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,29 +6,28 @@ import { createStackNavigator } from "@react-navigation/stack";
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.main}>
-      <span>
+      <span style={{alignSelf:"center"}}>
         <FontAwesome
           style={{ paddingRight: 100 }}
           name="map-marker"
-          size={99}
-          color="rgba(230, 200, 0, 0.90)"
+          size={75}
+          color="rgba(100, 100, 255, 0.80)"
         />
         <Text style={styles.container_top}>SG Trails</Text>
         <FontAwesome
           style={{ paddingLeft: 100 }}
           name="map-marker"
-          size={99}
-          color="rgba(230, 200, 0, 0.90)"
+          size={75}
+          color="rgba(100, 100, 255, 0.80)"
         />
       </span>
       <Image
         style={styles.profilePic}
-        source={{
-          uri: "https://www.clipartkey.com/mpngs/m/154-1545165_transparent-background-travel-icon-png.png",
-        }}
+        source={
+          require('./assets/SGTrails_logo.png')
+        }
       ></Image>
-      <Text style={styles.container_bottom}>News Feed</Text>
-      <View style={styles.rectangle}></View>
+      <Text style={styles.container_bottom}>News</Text>
     </View>
   );
 }
@@ -39,8 +38,8 @@ export default function HomeStack() {
     <Stack.Navigator>
       <Stack.Screen
         options={{
-          headerStyle: { backgroundColor: "lightyellow" },
-          headerTintColor: "tomato",
+          headerStyle: { backgroundColor: "" },
+          headerTintColor: "",
           headerTitleStyle: { fontWeight: "bold" },
         }}
         name="Home"
@@ -54,22 +53,21 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: "top",
-    alignItems: "center",
-    backgroundColor: "lightgreen",
+    backgroundColor: "lightblue",
     paddingTop: 10,
   },
   container_top: {
-    color: "#FF7F50",
+    color: "#5454FF",
     fontSize: 50,
     fontWeight: "bold",
     fontFamily: "Calibri",
   },
   container_bottom: {
-    color: "grey",
+    color: "blue",
     fontWeight: "bold",
     fontFamily: "Papyrus",
     fontStyle: "italic",
-    fontSize: 40,
+    fontSize: 30,
     marginTop: 30,
   },
   profilePic: {
